a7bcafbbe0496ff45b9b2003d3410d990e1a07c2
phpmoli <phpmoli@users.noreply.github.com>
2015-12-18 04:35:03 +0100

    fixing possible security hole: moving executable from user home to write-restricted /opt

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,9 +3,6 @@ echo 'deb http://ppa.launchpad.net/vikoadi/ppa/ubuntu xenial main' | sudo tee /e
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D05009CB
 sudo apt-get update
 sudo apt-get install kdeconnect indicator-kdeconnect
-path="$HOME/.config/google-chrome/NativeMessagingHosts"
-file="$path/hu.moli.kdeconnect.json"
-echo > "$file"
-cat hu.moli.kdeconnect.json | while read line ; do if [[ "$line" == *'$HOME'* ]] ; then echo "${line%\$HOME*}$HOME${line#*\$HOME}" >> "$file" ; else echo "$line" >> "$file" ; fi ; done
-cp hu.moli.kdeconnect.py "$path/"
-chmod u+x "$path/hu.moli.kdeconnect.py"
+sudo cp hu.moli.kdeconnect.py /opt/
+sudo chmod a+rx /opt/hu.moli.kdeconnect.py
+cp hu.moli.kdeconnect.json "$HOME/.config/google-chrome/NativeMessagingHosts/"
